7acc6a54ad88aa70a55098008dad13d12935c5ad
pipegonza00 <62079126+pipegonza00@users.noreply.github.com>
2023-11-14 16:15:48 -0500

    Add files via upload

--- elevator.py
+++ elevator.py
@@ -6,7 +6,8 @@ def elevator(floors, actual, requests, dir):
     print("Direccion: ", dir)
 
     while len(floors) > 0:
-
+        
+        #Movimiento del ascensor
         while(actual not in floors):
             print("Elevador en piso", actual)
             print("Elevator", dir)
@@ -15,23 +16,22 @@ def elevator(floors, actual, requests, dir):
             else:
                 actual -= 1
 
+        #el ascensor se detiene en uno de los pisos pendientes
         print("elevador se detiene en piso", actual)
         floors.remove(actual)
         
+        #se mira si en el piso actual se solicita otro piso
         if actual in requests:
             print("Piso seleccionado", requests[actual])
+            #se agrega la solicitud a los pisos pendientes si no esta ya en estos.
             if requests[actual] not in floors:
                 floors.append(requests[actual])
             print(floors)
 
+        #se revisa que direccion debe seguir el ascensor 
         if len(floors) > 0:
             if actual > floors[0]:
                 dir = "Bajando"
-        
-
-
-        
-
 
 def main():
     floors = [5, 29, 13, 10]
